1b816717d3c04088ddf3cf992de10f96364f470b
Ajay Menon <ajaymenon0@gmail.com>
2017-03-04 18:30:30 +0530

    Update AlgorithmicCrush.js

--- AlgorithmicCrush.js
+++ AlgorithmicCrush.js
@@ -3,15 +3,22 @@ function processData(input) {
         word = lines[0].split(' '),
         N = parseInt(word[0]),
         M = parseInt(word[1]),
-        arr = new Array(N).fill(0)
+        arr = new Array(N+1).fill(0),
+        x = 0,
+        max = 0
     for(var i=1;i<=M;i++){
-        var q = lines[i].split(' '),
+       var q = lines[i].split(' '),
             a = parseInt(q[0])-1,
             b = parseInt(q[1])-1,
             k = parseInt(q[2])
-        for(var j=a;j<=b;j++){
-            arr[j] += k
-        }
+        arr[a] += k
+        if((b+1)<=N) arr[b+1] -= k
     }
-    console.log(Math.max(...arr))
-}
+    for(var i=0;i<=N;i++){
+        x = x + arr[i]
+        if(x>max) max=x
+    }
+    console.log(max)
+} 
+//Couldn't crack this one myself. 
+//I thank: http://codereview.stackexchange.com/questions/95755/algorithmic-crush-problem-hitting-timeout-errors
